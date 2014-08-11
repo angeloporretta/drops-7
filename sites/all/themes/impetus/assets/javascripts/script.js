0e28f37678ce45f6d5883634beb5ad1fbc1d7f0b
Angelo <angelo@ubuntu.(none)>
2014-08-11 16:26:09 -0400

    Styling for new dashboard menu

--- sites/all/themes/impetus/assets/javascripts/script.js
+++ sites/all/themes/impetus/assets/javascripts/script.js
@@ -29,5 +29,13 @@
         }
       });
     }
+    
+    // Handle dashboard space dropdown menu.
+    $('#panels-ipe-regionid-sidebar .spaces-section-dropdown > li').click(function(){
+      if ($(this).children('.nested').hasClass('show')) {
+        $('#panels-ipe-regionid-sidebar .spaces-section-dropdown > li').removeClass('open');
+        $(this).addClass('open');
+      }
+    });
   });
 })(jQuery);
