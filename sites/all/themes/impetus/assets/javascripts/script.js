46124ff2708534067e0a28d1059a653e2b0b9fc6
Angelo <angelo@ubuntu.(none)>
2014-08-14 12:15:27 -0400

    Styling changes from natalie

--- sites/all/themes/impetus/assets/javascripts/script.js
+++ sites/all/themes/impetus/assets/javascripts/script.js
@@ -19,16 +19,25 @@
     // Handle auto-expansion of oa group menu.
     $('.pane-og-menu-single-menu ul li').removeClass('expanded').addClass('collapsed');
     $('.pane-og-menu-single-menu ul li').find('ul').css('display', 'none');
-    
-    var nestedFormItem = $('.pane-og-menu-single-menu ul li ul').find('a.active');
-    if (nestedFormItem.length > 0) {
-      var menuToggleButtons = $(nestedFormItem).parents('ul.menu').siblings('a').children('.dhtml-menu-icon');
-      $(menuToggleButtons).each(function(){
-        if ($(this).parent('a').parent('li').hasClass('collapsed')) {
-          $(this).click();
-        }
-      });
+    if ($('body.node-type-oa-section').length < 1) {
+
+      var nestedFormItem = $('.pane-og-menu-single-menu ul li ul').find('a.active');
+      if (nestedFormItem.length > 0) {
+        var menuToggleButtons = $(nestedFormItem).parents('ul.menu').siblings('a').children('.dhtml-menu-icon');
+        $(menuToggleButtons).each(function(){
+          if ($(this).parent('a').parent('li').hasClass('collapsed')) {
+            $(this).click();
+          }
+        });
+      }
     }
+    else {
+      $('.pane-og-menu-single-menu ul a.active .dhtml-menu-icon').click();
+    }
+    
+    // Other styling for oa group menu.
+    $('.content .pane-og-menu-single-menu ul a.active').siblings('ul').find('a').css('color', '#333');
+    $('.content .pane-og-menu-single-menu ul .expanded a').css('color', '#333');
     
     // Handle dashboard space dropdown menu.
     $('.content .spaces-section-dropdown > li').click(function(){
