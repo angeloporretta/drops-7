f5819ca883068fceb3dd05d7797a2ac7ce4666cd
Angelo <angelo@ubuntu.(none)>
2014-08-22 17:04:30 -0400

    Checking the do not send notifications input by default

--- sites/all/modules/custom/form_modifier/form_modifier.js
+++ sites/all/modules/custom/form_modifier/form_modifier.js
@@ -6,6 +6,12 @@
         $(this).find('label').text(Drupal.t('Groups That Have Access'));
         $(this).find('.description').text('');
       });
+      
+      $('.node-form #edit-oa-notifications-notifications-skip-notify', context).once('changeLabels', function () {
+        if ($(this).not(':checked')) {
+          $(this).click();
+        }
+      });
     }
   };
 })(jQuery);
