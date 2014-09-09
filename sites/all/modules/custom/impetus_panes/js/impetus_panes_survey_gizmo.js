533f2960e74aa350d60798cf492f30881ec7121f
Angelo <angelo@ubuntu.(none)>
2014-09-09 10:44:10 -0400

    Adding proper verbiage for survey gizmo prefills and ability to optionally hide prefilled fields.

--- sites/all/modules/custom/impetus_panes/js/impetus_panes_survey_gizmo.js
+++ sites/all/modules/custom/impetus_panes/js/impetus_panes_survey_gizmo.js
@@ -5,26 +5,36 @@
       var firstName = Drupal.settings.impetusPanes.firstName;
       var lastName = Drupal.settings.impetusPanes.lastName;
       var email = Drupal.settings.impetusPanes.email;
+      var isHidden = Drupal.settings.impetusPanes.prefillsHidden;
       
       setTimeout(function(){
         
         $('.sg-question-title').each(function(index, element){
           var label = $(element).find('label').text();
+          var fieldDisplay = 'block';
           
-          if (label.match(/First Name/i)) {
-            $(element).siblings('.sg-question-options').find('input[type="text"]').val(firstName);
+          if (isHidden) {
+            fieldDisplay = 'none';
           }
-          else if (label.match(/Last Name/i)) {
+          
+          if (label.match(/Your First Name/i)) {
+            $(element).siblings('.sg-question-options').find('input[type="text"]').val(firstName);
+            $(element).parents('.sg-type-textbox').css('display', fieldDisplay);
+          }
+          else if (label.match(/Your Last Name/i)) {
             $(element).siblings('.sg-question-options').find('input[type="text"]').val(lastName);
+            $(element).parents('.sg-type-textbox').css('display', fieldDisplay);
           }
           else if (label.match(/Your Name/i)) {
             $(element).siblings('.sg-question-options').find('input[type="text"]').val(firstName + ' ' + lastName);
+            $(element).parents('.sg-type-textbox').css('display', fieldDisplay);
           }
-          else if (label.match(/mail/i)) {
+          else if (label.match(/Your Email/i)) {
             $(element).siblings('.sg-question-options').find('input').val(email);
+            $(element).parents('.sg-type-textbox').css('display', fieldDisplay);
           }
         }); 
-      }, 1250);
+      }, 1750);
       
     }
   };
