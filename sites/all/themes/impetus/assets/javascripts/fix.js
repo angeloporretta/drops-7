3d36c4962f3ba54bcfc6362e67ebf0802717cf70
Adrian Ababei <adiababei2@gmail.com>
2014-11-14 18:33:37 +0200

    Fix menu issue

--- sites/all/themes/impetus/assets/javascripts/fix.js
+++ sites/all/themes/impetus/assets/javascripts/fix.js
@@ -2,10 +2,14 @@ jQuery( window ).load(function() {
   //var active = jQuery('ul.menu a.active');
   var count = 0;
   jQuery("ul.menu li.dhtml-menu a.active").each(function() {
+  	var test = jQuery(this);
   	var parent = jQuery(this).parent().parent().parent();
   	var item = jQuery(parent[0]).children();
-  	var icon = jQuery(item[0]).children();
-  	jQuery(icon[0]).click();
+  	if (test[0].childNodes.length > 0) {
+  	if (parent.hasClass("collapsed")) {
+  	  var icon = jQuery(item[0]).children();
+  	  jQuery(icon[0]).click();
+  	} }
   });
 
 });
