ac2cfe2eceae3decfd1aa818c0048117022aaf63
Jess Hunte <jess.hunte@opin.ca>
2014-11-19 11:01:28 -0500

    Adding panel custom style module.

---
+++ sites/all/modules/custom/panel_custom_style/plugins/styles/custom_attributes/custom-attributes.tpl.php
@@ -0,0 +1,10 @@
+<?php //echo "<pre>" . print_r($variables,true) . "</pre>"; ?>
+<div class="<?php print $classes; ?>" <?php print $id; ?> <?php print drupal_attributes($style_attributes); ?>>
+    <?php if ($title): ?>
+        <h3<?php print $title_attributes; ?> <?php print drupal_attributes($title_style); ?>><?php print $title; ?></h3>
+    <?php endif; ?>
+    <div class="pane-content">
+        <?php print render($content); ?>
+    </div>
+
+</div>
