f2a2a81f92a21c8b7be6bf973e3590c0420ea19f
Adrian Ababei <adiababei2@gmail.com>
2014-12-05 20:19:22 +0200

    Fix top menu to expand on hover and not on click

--- sites/all/modules/custom/impetus_panes/js/impetus_panes_section_dropdown.js
+++ sites/all/modules/custom/impetus_panes/js/impetus_panes_section_dropdown.js
@@ -3,21 +3,31 @@
   Drupal.behaviors.dropdownSections = {
     attach: function (context, settings) {
       
-      $('.spaces-section-dropdown .dropdown a', context).once('dropdownSections', function () {
+      if ($('body.mobile').length === 0) {
+      $('#mini-panel-oa_toolbar_panel .spaces-section-dropdown > li > a').hover(function(){
         
-        $(this).click(function(e){
-          if ($(this).siblings('ul.nested').css('display') == 'none') {
-            e.preventDefault();
-            $('.spaces-section-dropdown .nested').addClass('hide').removeClass('show');
-            $(this).siblings('ul.nested').addClass('show');
+        $('#mini-panel-oa_toolbar_panel .spaces-section-dropdown .nested').addClass('hide').removeClass('show');
+        var nestedMenu = $(this).siblings('ul.nested');
+        $(nestedMenu).removeClass('hide').addClass('show');
+      },
+      function(){
+        var nestedMenu = $(this).siblings('ul.nested');
+        var hideTimeout = setTimeout(function(){
+          $(nestedMenu).addClass('hide').removeClass('show');
+        }, 1000);
+        
+        $(nestedMenu).hover(
+          function(){
+            clearTimeout(hideTimeout);
+          },
+          function(){
+            setTimeout(function(){
+              $(nestedMenu).addClass('hide').removeClass('show');
+            }, 500);
           }
-        });
-       
-        $('.spaces-section-dropdown .nested .close-button button').click(function(){
-          $('.spaces-section-dropdown .nested').addClass('hide').removeClass('show');
-        });
-        
+        );
       });
+    } 
     }
   };
 })(jQuery);
