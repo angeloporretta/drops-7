6b2c2114c914575bfa68af4df0ffa4d3bb2a06f0
Adrian Ababei <adiababei2@gmail.com>
2014-12-09 13:25:29 +0200

    Fix sidebar menu issue(not all parents would expand)

--- sites/all/themes/impetus/assets/javascripts/fix.js
+++ sites/all/themes/impetus/assets/javascripts/fix.js
@@ -1,15 +1,28 @@
 jQuery( window ).load(function() {
-  //var active = jQuery('ul.menu a.active');
+  //add class active to li
+  var active_item = jQuery("..panels-ipe-portlet-wrapper .pane-og-menu-single-menu .pane-content ul.menu li a.active");
+  if (typeof active_item[0] != 'undefined') {
+    jQuery(active_item[0]).parent().addClass('active');
+  }
   var count = 0;
   jQuery("ul.menu li.dhtml-menu a.active").each(function() {
   	var test = jQuery(this);
-  	var parent = jQuery(this).parent().parent().parent();
-  	var item = jQuery(parent[0]).children();
-  	if (test[0].childNodes.length > 0) {
-  	if (parent.hasClass("collapsed")) {
-  	  var icon = jQuery(item[0]).children();
-  	  jQuery(icon[0]).click();
-  	} }
+    var parents = test.parents('.collapsed');
+    var i;
+
+    jQuery.each(parents , function(i , l){
+         if (jQuery(l).hasClass("collapsed")) {
+         //get childrens of li
+         var children = jQuery(jQuery(l)[0]).children();
+         //get childrens of a
+         var get_children = jQuery(children.children(".dhtml-menu-icon"));
+         //click only the items that actually have an icon next to them
+         if (get_children.length > 0) {
+            get_children[0].click();
+         }
+       }
+    });
+    
   });
 
 });
