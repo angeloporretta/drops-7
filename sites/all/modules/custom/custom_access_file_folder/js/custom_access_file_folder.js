361db954c3cf97f6a997476b9c9758f96e4eaef6
Adrian Ababei <adiababei2@gmail.com>
2014-12-16 12:47:25 +0200

    File/Folder access capabilities

---
+++ sites/all/modules/custom/custom_access_file_folder/js/custom_access_file_folder.js
@@ -0,0 +1,7 @@
+jQuery( document ).ready(function() {
+  var ajax_textfield = jQuery(".form-item-users");
+  var textfield = jQuery(".form-item-users")[0]
+  jQuery('#edit-add-more').click(function(event){
+  	event.preventDefault();
+  });
+});
