202d4ced54d09977662c4f07dc1cf2afb00a50b9
Adrian Ababei <adiababei2@gmail.com>
2015-01-09 09:43:31 +0200

    Fix bug that would hide the top menu bar on some pages

--- sites/all/themes/impetus/assets/javascripts/remove-lightbox-menu.js
+++ sites/all/themes/impetus/assets/javascripts/remove-lightbox-menu.js
@@ -1,14 +1,16 @@
 jQuery( document ).ready(function() {
-    
-    var iframe = jQuery("iframe");
-    if (iframe.length > 0) {
-      var iframe_body = jQuery("#oa-navbar");
+
+     jQuery("a").click(function() {
+     jQuery("#lightboxFrame").on("load" , function(){
+    var iframe_body = jQuery("#lightboxFrame").contents().find("#oa-navbar");
+     if (iframe_body.length > 0) {
       iframe_body.css("display" , "none");
-      var body = jQuery("body");
+      var body = jQuery("#lightboxFrame").contents().find("body");
       body.css("margin-top" , 0);
       body.css("padding-top" , 0);
-      var main = jQuery("#main-wrapper");
+      var main = jQuery("#lightboxFrame").contents().find("#main-wrapper");
       main.css("margin-top" , 0);
-  	}
-
+     }
+  });
+     });
 });
