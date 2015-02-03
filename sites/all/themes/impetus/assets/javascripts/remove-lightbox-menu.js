6f762f713e4b66d9a4b470431c68957ff2bb925c
Adrian Ababei <adiababei2@gmail.com>
2015-02-03 12:29:35 +0200

    remove footer and contact button from lightboxes

--- sites/all/themes/impetus/assets/javascripts/remove-lightbox-menu.js
+++ sites/all/themes/impetus/assets/javascripts/remove-lightbox-menu.js
@@ -1,7 +1,16 @@
 jQuery( document ).ready(function() {
 
      jQuery("a").click(function() {
+      
      jQuery("#lightboxFrame").on("load" , function(){
+      var iframe_footer = jQuery("#lightboxFrame").contents().find("#footer");
+     if (iframe_footer.length > 0) {
+      iframe_footer.css("display" , "none");
+     }
+      var iframe_support = jQuery("#lightboxFrame").contents().find("#support-callout-container");
+      if (iframe_support.length > 0) {
+        iframe_support.css("display" , "none");
+      }
     var iframe_body = jQuery("#lightboxFrame").contents().find("#oa-navbar");
      if (iframe_body.length > 0) {
       iframe_body.css("display" , "none");
@@ -11,6 +20,7 @@ jQuery( document ).ready(function() {
       var main = jQuery("#lightboxFrame").contents().find("#main-wrapper");
       main.css("margin-top" , 0);
      }
+
   });
      });
 });
