6186ca38cc323c230c588b9dfe90fbffe7c6e0a2
Adrian Ababei <adiababei2@gmail.com>
2015-02-03 13:34:17 +0200

    line up sidebar with title

--- sites/all/themes/impetus/assets/javascripts/remove-white-space.js
+++ sites/all/themes/impetus/assets/javascripts/remove-white-space.js
@@ -1,11 +1,11 @@
 jQuery( document ).ready(function() {
   setTimeout(function(){
     var navbar = jQuery(".navbar-fixed-top").outerHeight(true);
-    jQuery("body").css("margin-top" , navbar+30);
-    jQuery("body").css("padding-top" , 0);
+      jQuery("body").css("margin-top" , navbar+30);
+      jQuery("body").css("padding-top" , 0);
   } , 1000);
-  var title = document.getElementById("page-title");
-  if (title != null) {
+  var title = jQuery("#page-title");
+  if (title.css("display") != "none") {
   	var sidebar = jQuery(".burr-flipped-sidebar");
   	sidebar.css("margin-top" , -40);
   }
