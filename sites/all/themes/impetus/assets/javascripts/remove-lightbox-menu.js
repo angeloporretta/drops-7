1a66fb22920e6e132d264315c579f35e0770f0df
Adrian Ababei <adiababei2@gmail.com>
2015-11-09 16:19:27 +0200

    fix comments issue

--- sites/all/themes/impetus/assets/javascripts/remove-lightbox-menu.js
+++ sites/all/themes/impetus/assets/javascripts/remove-lightbox-menu.js
@@ -1,3 +1,68 @@
+(function($) {    
+  if ($.fn.style) {
+    return;
+  }
+
+  // Escape regex chars with \
+  var escape = function(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+  };
+
+  // For those who need them (< IE 9), add support for CSS functions
+  var isStyleFuncSupported = !!CSSStyleDeclaration.prototype.getPropertyValue;
+  if (!isStyleFuncSupported) {
+    CSSStyleDeclaration.prototype.getPropertyValue = function(a) {
+      return this.getAttribute(a);
+    };
+    CSSStyleDeclaration.prototype.setProperty = function(styleName, value, priority) {
+      this.setAttribute(styleName, value);
+      var priority = typeof priority != 'undefined' ? priority : '';
+      if (priority != '') {
+        // Add priority manually
+        var rule = new RegExp(escape(styleName) + '\\s*:\\s*' + escape(value) +
+            '(\\s*;)?', 'gmi');
+        this.cssText =
+            this.cssText.replace(rule, styleName + ': ' + value + ' !' + priority + ';');
+      }
+    };
+    CSSStyleDeclaration.prototype.removeProperty = function(a) {
+      return this.removeAttribute(a);
+    };
+    CSSStyleDeclaration.prototype.getPropertyPriority = function(styleName) {
+      var rule = new RegExp(escape(styleName) + '\\s*:\\s*[^\\s]*\\s*!important(\\s*;)?',
+          'gmi');
+      return rule.test(this.cssText) ? 'important' : '';
+    }
+  }
+
+  // The style function
+  $.fn.style = function(styleName, value, priority) {
+    // DOM node
+    var node = this.get(0);
+    // Ensure we have a DOM node
+    if (typeof node == 'undefined') {
+      return this;
+    }
+    // CSSStyleDeclaration
+    var style = this.get(0).style;
+    // Getter/Setter
+    if (typeof styleName != 'undefined') {
+      if (typeof value != 'undefined') {
+        // Set style property
+        priority = typeof priority != 'undefined' ? priority : '';
+        style.setProperty(styleName, value, priority);
+        return this;
+      } else {
+        // Get style property
+        return style.getPropertyValue(styleName);
+      }
+    } else {
+      // Get CSSStyleDeclaration
+      return style;
+    }
+  };
+})(jQuery);
+
 jQuery( document ).ready(function() {
 
      jQuery("a").click(function() {
@@ -15,7 +80,12 @@ jQuery( document ).ready(function() {
      if (iframe_body.length > 0) {
       iframe_body.css("display" , "none");
       var body = jQuery("#lightboxFrame").contents().find("body");
-      body.attr('style' , "margin-top: 0px !important");
+      setTimeout(function(){
+        
+        body.css("margin-top" , 0);
+      } , 1000);
+      // body.each(function () { this.style.setProperty( 'border-top', '0px', 'important' ); });
+      body.addClass("remove-margin");
       body.css("padding-top" , 0);
       var main = jQuery("#lightboxFrame").contents().find("#main-wrapper");
       main.css("margin-top" , 0);
--- sites/all/themes/impetus/assets/javascripts/remove-white-space.js
+++ sites/all/themes/impetus/assets/javascripts/remove-white-space.js
@@ -3,10 +3,10 @@ jQuery( document ).ready(function() {
     var navbar = jQuery(".navbar-fixed-top").outerHeight(true);
       jQuery("body").css("margin-top" , navbar+30);
       jQuery("body").css("padding-top" , 0);
-  } , 1000);
+  } , 50);
   var title = jQuery("#page-title");
   if (title.length > 0) {
-  	var sidebar = jQuery("#panels-ipe-regionid-sidebar");
+  	var sidebar = jQuery(".burr-flipped-sidebar");
   	sidebar.css("margin-top" , -40);
   }
 });
